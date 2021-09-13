a259953f1d21e2c8e81edcc3360ed2008a5ec4ee
Shamibei <shannel.mibei@student.moringaschool.com>
2021-09-13 15:40:03 +0300

    initial js commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,42 @@
 $(document).ready(function(){
 
+  $("#icon1>img").click(function(){
+    $("#icon1>img").toggle().hide();
+    $("#text1").show();
+  });
+  $("#text1").click(function(){
+    $("#text1").hide();
+    $("#icon1>img").slideDown().show();
+  });
 
-  $('#text').hide();
-
-  $('#text2').hide();
-
-  $('#text3').hide();
 
 
-});    
+  $("#icon2>img").click(function(){
+    $("#icon2>img").toggle().hide();
+    $("#text2").show();
+  });
+  $("#text2").click(function(){
+    $("#text2").hide();
+    $("#icon2>img").slideDown().show();
+  });
+
+
+
+  $("#icon3>img").click(function(){
+    $("#icon3>img").toggle().hide();
+    $("#text3").show();
+  });
+  $("#text3").click(function(){
+    $("#text3").hide();
+    $("#icon3>img").slideDown().show();
+  });
+
+
+
+
+
+
+
+
+});
+  
