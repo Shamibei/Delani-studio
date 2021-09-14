33b6aa2f5c731fa4b7e97c96e7efb071332a6a99
Shamibei <shannel.mibei@student.moringaschool.com>
2021-09-14 09:01:04 +0300

    hide text

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
+
+$("#text1").hide();
   $("#icon1>img").click(function(){
-    $("#icon1>img").toggle().hide();
+    $("#icon1>img").slideDown().hide();
     $("#text1").show();
   });
   $("#text1").click(function(){
@@ -10,9 +12,9 @@ $(document).ready(function(){
   });
 
 
-
+$("#text2").hide();
   $("#icon2>img").click(function(){
-    $("#icon2>img").toggle().hide();
+    $("#icon2>img").slideDown().hide();
     $("#text2").show();
   });
   $("#text2").click(function(){
@@ -21,9 +23,9 @@ $(document).ready(function(){
   });
 
 
-
+$("#text3").hide();
   $("#icon3>img").click(function(){
-    $("#icon3>img").toggle().hide();
+    $("#icon3>img").slideDown().hide();
     $("#text3").show();
   });
   $("#text3").click(function(){
@@ -49,7 +51,16 @@ const contact= (e) =>{
 
   const userMessage = frm.get("message")
 
-  
+  const userSubmit = frm.get("submit")
+
+  addEventListener("submit");
+
+  if(userName+userMessage+userEmail+userSubmit){
+    alert(userName+" , We have received your message . Thank you for contacting us.")
+  }else{
+    alert("Please provide all the data required")
+  };
+
 
 
 }
